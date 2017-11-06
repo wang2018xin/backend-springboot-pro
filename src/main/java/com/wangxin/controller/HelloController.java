bb6993f98b36e3c136efbb6b5e530000407d7b7f
wang2018xin <wang2018xin@163.com>
2017-11-06 16:15:21 +0800

    读取配置文件（yml）

--- src/main/java/com/wangxin/controller/HelloController.java
+++ src/main/java/com/wangxin/controller/HelloController.java
@@ -1,13 +1,19 @@
 package com.wangxin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wangxin.pojo.Bar;
+
 @RestController
 @EnableAutoConfiguration
 public class HelloController {
+
+	@Autowired
+	private Bar bar;
 
 	@RequestMapping("/")
 	String home() {
@@ -17,5 +23,13 @@ public class HelloController {
 	@RequestMapping("/hello/{myName}")
 	String index(@PathVariable String myName) {
 		return "Hello " + myName + "!!!";
+	}
+
+	/**
+	 * 读取配置文件(application-test.yml)
+	 */
+	@RequestMapping("/bar")
+	String getConf() {
+		return bar.toString();
 	}
 }
